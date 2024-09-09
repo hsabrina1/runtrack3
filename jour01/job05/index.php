fc4ae746e86b98057e745ece414a18d03241ee53
hsabrina1 <hadi.sabrina@hotmail.com>
2024-09-09 11:25:08 +0200

    job05

---
+++ jour01/job05/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jours de la Semaine</title>
+    <script src="script.js"></script> 
+</head>
+<body>
+
+    <script>
+        // Appel de la fonction "afficherjourssemaines" pour afficher les jours de la semaine
+        afficherjourssemaines();
+    </script>
+
+</body>
+</html>
---
+++ jour01/job05/script.js
@@ -0,0 +1,9 @@
+function afficherjourssemaines() {
+    // Déclare un tableau contenant les jours de la semaine
+    var jourssemaines = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+
+    // Utilisation d'une boucle for pour parcourir le tableau et afficher chaque jour
+    for (var i = 0; i < jourssemaines.length; i++) {
+        console.log(jourssemaines[i]); // Affichage dans la console
+    }
+}
