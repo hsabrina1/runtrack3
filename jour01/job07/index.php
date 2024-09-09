559d354410f15c628b7cd2b7947e46312bfd2d57
hsabrina1 <hadi.sabrina@hotmail.com>
2024-09-09 11:55:40 +0200

    job07

--- jour01/job07/index.php
+++ jour01/job07/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vérification des jours travaillés</title>
-    <script src="script.js"></script> <!-- Lien vers le fichier script.js -->
+    <script src="script.js"></script>
 </head>
 <body>
 
