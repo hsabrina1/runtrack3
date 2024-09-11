d33f430eb2f5e42dc544226a2c28c5b55fbefbe1
hsabrina1 <hadi.sabrina@hotmail.com>
2024-09-11 09:48:28 +0200

    job02

--- jour02/job06/index.php
+++ jour02/job06/index.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css?t=<?php echo time(); ?>">
+    
     <script src="script.js" defer></script>
 </head>
 <body>
